98e2f9df16fd90a6d9b647e4a1a4257d35b35d40
shmyair <shimizu@air.co.jp>
2019-10-23 19:36:31 +0900

    [add] run batch

---
+++ talend/readExcel_report/run1.bat
@@ -0,0 +1,2 @@
+
+./readExcel_report/readExcel_report_run.bat --context_param report_path="C:/Users/AIR1175/Downloads/‰Ò“­"
---
+++ talend/readExcel_report/run2.bat
@@ -0,0 +1,2 @@
+
+./readExcel_report/readExcel_report_run.bat --context_param report_path="C:/Users/AIR1175/株式会社エアー/口田 光緒 - デ活SES/稼働管理/2019/10"
